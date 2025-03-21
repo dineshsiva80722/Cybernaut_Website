c2c230c9bf1648549239c3d4581b2e5b2fb136ab
dineshsiva <dineshsiva693@example.com>
2025-03-21 15:13:02 +0530

    home page updated

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+      'res.cloudinary.com'
+    ]
+  }
+};
+
+module.exports = nextConfig;
